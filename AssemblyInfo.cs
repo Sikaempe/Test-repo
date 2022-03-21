26208e396e9dd7287eed3921a445a6e521cdd3d3
BOP BuildBot <hudson-nlt.tech@btc-ag.com>
2022-03-21 16:20:39 +0000

    inc. support branch version

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -18,5 +18,5 @@ using System.Reflection;
 // by using the '*' as shown below:
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyVersion("0.10.0.0")]
-[assembly: AssemblyFileVersion("0.10.0.0")]
-[assembly: AssemblyInformationalVersion("0.10.0.0")]
+[assembly: AssemblyFileVersion("0.10.1.0")]
+[assembly: AssemblyInformationalVersion("0.10.1.0")]
