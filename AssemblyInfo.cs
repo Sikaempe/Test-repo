26e1e360e188beb6fd027b9fbc5d946d75b7f267
BOP BuildBot <hudson-nlt.tech@btc-ag.com>
2022-03-21 16:19:26 +0000

    Started Release for 0.9.0

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -19,4 +19,4 @@ using System.Reflection;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyVersion("0.9.0.0")]
 [assembly: AssemblyFileVersion("0.9.0.0")]
-[assembly: AssemblyInformationalVersion("0.9.0.0-testing")]
+[assembly: AssemblyInformationalVersion("0.9.0.0")]
