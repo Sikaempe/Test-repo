4d2a8ed52b1fcb1b2f3a8dbab1cefff001f67a23
Simon Kaemper <SIMON.KAEMPER@BTC-AG.COM>
2024-09-04 13:15:25 +0200

    Jenkinsfile.groovy edited online with Bitbucket
    Jenkins Datei Update

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,24 +1,79 @@
-@Library(["vpp", "bop@auto-channel","bop-release"]) _
+pipeline {
+    agent any
 
+    environment {
+        BITBUCKET_URL = 'http://bitbucket:7990/scm/sync/syncme.git'
+        GITHUB_REPO_URL = 'https://github.com/Sikaempe/test-repo.git'
+    }
 
-    stage('TestGit') 
-    {
-        util.AutoChannel.isGitFlow = true
+    stages {
+        stage('Cleanup Workspace') {
+            steps {
+                script {
+                    echo "Cleaning up the workspace to avoid directory conflicts..."
+                    deleteDir() // This will delete the contents of the workspace
+                }
+            }
+        }
 
-        def xChannel = auto_channel()
-        
-        echo "channel: " + xChannel
+        stage('Clone Bitbucket Repository') {
+            steps {
+                withCredentials([usernamePassword(credentialsId: 'bitbucket-token', usernameVariable: 'BITBUCKET_USERNAME', passwordVariable: 'BITBUCKET_TOKEN')]) {
+                    script {
+                        echo "Cloning Bitbucket repository..."
+                        sh """
+                            git clone https://$BITBUCKET_USERNAME:$BITBUCKET_TOKEN@$BITBUCKET_URL
+                        """
+                    }
+                }
+            }
+        }
 
-        if (xChannel.equals(util.AutoChannel.TESTING)) 
-        {
-            echo "testing"
-        } 
-        else if (xChannel.equals(util.AutoChannel.STABLE)) 
-        {
-            echo "stable"
-        } 
-        else 
-        {
-            echo "other"
+        stage('Add GitHub Remote') {
+            steps {
+                withCredentials([usernamePassword(credentialsId: 'github-token', usernameVariable: 'GITHUB_USERNAME', passwordVariable: 'GITHUB_TOKEN')]) {
+                    script {
+                        echo "Adding GitHub remote..."
+                        sh """
+                            cd syncme
+                            git remote add github https://$GITHUB_USERNAME:$GITHUB_TOKEN@$GITHUB_REPO_URL
+                        """
+                    }
+                }
+            }
+        }
+
+        stage('Push All Branches to GitHub') {
+            steps {
+                script {
+                    echo "Pushing all branches to GitHub..."
+                    sh """
+                        cd syncme
+                        git push github --all
+                    """
+                }
+            }
+        }
+
+        stage('Push All Tags to GitHub') {
+            steps {
+                script {
+                    echo "Pushing all tags to GitHub..."
+                    sh """
+                        cd syncme
+                        git push github --tags
+                    """
+                }
+            }
         }
     }
+
+    post {
+        success {
+            echo "Bitbucket repository successfully synced to GitHub."
+        }
+        failure {
+            echo "Failed to sync Bitbucket repository to GitHub."
+        }
+    }
+}
